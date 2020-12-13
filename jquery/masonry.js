aa101e83a5fb7ed58c49f1659a1f9e265a021fa8
Ylubinets <lubinec2@gmail.com>
2020-12-13 21:07:03 +0200

    Fixed
    Fixed

--- jquery/masonry.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const masonry = document.querySelector('.grid-gallery-list');
-
-const msnr = new Masonry(masonry, {
-    itemSelector: '.grid-gallery-item',
-    columnWidth: 370,
-    gutter: 21,
-});
